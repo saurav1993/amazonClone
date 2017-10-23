24207ec64d0aab81c23ebb21fec1aff4da95dc45
Saurav <surgang81993@gmail.com>
2017-10-24 00:26:46 +0530

    upto 2.16

--- models/user.js
+++ models/user.js
@@ -19,16 +19,13 @@ var UserSchema = new Schema({
     }
 })
 
-
-
-
 //Hash the password
 UserSchema.pre('save',function(next){
     var user = this;
     if(!user.isModified('password')) return next();
     bcrypt.genSalt(10,function(err,salt){
         if(err) return next(err);
-        bcrypt.hash(user.password, salt,null,function(){
+        bcrypt.hash(user.password, salt,null,function(err,hash){
             if(err) return next(err);
             user.password = hash;
             next();
@@ -41,3 +38,5 @@ UserSchema.pre('save',function(next){
 UserSchema.method.comparePassword = function(password){
     return bcrypt.compareSync(password,this.password);
 }
+
+module.exports = mongoose.model('User',UserSchema);
--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+
+const User = require('./models/user');
 
 const app = express();
 
+mongoose.connect('mongodb://root:12345@ds227035.mlab.com:27035/ecommerce',function(err){
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log("connected to the database");
+    }
+});
+
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.engine('ejs',ejsMate);
+app.set('view engine','ejs');
 
-//Home Route
-app.get("/",(req,res)=>{
-    res.send("Hi");
-})
+app.post('/create-user',function(req,res,next){
+    var user = new User();
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    user.save(function(err){
+        if(err) return next(err);
+        res.json("Successfully added a new user");
+    });
+});
+
+app.get('/',function(req,res){
+    res.render("home");
+});
+
+app.get('/about',function(req,res){
+    res.render("about");
+});
 
 app.listen(4000,function(err){
     if(err) throw err;
